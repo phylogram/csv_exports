efc66e74eb4a8435a59461cb2cc00bc01030d983
phylogram <philip.roeggla@reflex.at>
2018-04-19 17:00:24 +0200

    rm temp files

--- design/opt_in_contact.sql
+++
@@ -1,8 +0,0 @@
-SELECT *
-  FROM redhen_contact
-  JOIN (campaignion_activity, campaignion_activity_newsletter_subscription)
-    ON (redhen_contact.contact_id = campaignion_activity.contact_id
-	   AND campaignion_activity.activity_id = campaignion_activity_newsletter_subscription.activity_id
-        )
-
- 
